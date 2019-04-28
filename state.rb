95f0b5a1953b7185fa27aa1abfdcc612c4832929
PauloVivas <paulovivas@id.uff.br>
2019-04-27 21:07:37 -0300

    corrigindo um milhao de erros no state com array de pieces

--- state.rb
+++ state.rb
@@ -1,93 +1,96 @@
 require_relative "piece"
 class State
   
-  attr_accessor :pieces_player1, :pieces_player2 :turn,  
+  attr_accessor :pieces_player1, :pieces_player2, :turn,  
                 :turns_without_promotion, :turns_without_points  
   
   def initialize(initial_player)
    
+    @pieces_player1 = []
+    @pieces_player2 = []
+
     #second player pieces
     piece1 = Piece.new(2, 0, 1)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 0, 3)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 0, 5)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 0, 7)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
 
     piece1 = Piece.new(2, 1, 0)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 1, 2)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 1, 4)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 1, 6)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
 
     piece1 = Piece.new(2, 2, 1)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 2, 3)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 2, 5)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     piece1 = Piece.new(2, 2, 7)
-    pieces_player2 << piece1
+    @pieces_player2 << piece1
 
     #first player pieces    
     piece1 = Piece.new(1, 7, 0)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 7, 2)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 7, 4)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 7, 6)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
 
     piece1 = Piece.new(1, 6, 1)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 6, 3)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 6, 5)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 6, 7)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     
     piece1 = Piece.new(1, 5, 0)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 5, 2)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 5, 4)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     piece1 = Piece.new(1, 5, 6)
-    pieces_player1 << piece1
+    @pieces_player1 << piece1
 
     
     @turn = initial_player
-    turns_without_promotion = 0
-    turns_without_points = 0
+    @turns_without_promotion = 0
+    @turns_without_points = 0
   
   end
 
